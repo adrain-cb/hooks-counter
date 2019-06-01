e08c76e7971b404f65bdf25a75b969110142d529
Adrian Smith <adriansmith@Ryans-MBP-2.localdomain>
2019-05-31 17:54:42 -0700

    Implement random color generation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [state, setState] = useState({
-    count: 0
-  })
+const App = props => {
+  // Using more than necessary state to test hooks
+  const [count, setCount] = useState(0);
+  const [randColor, setRandColor] = useState("");
+  const [randInt, setRandInt] = useState(0);
+
+  const generateRandInt = () => {
+    let randInt = Math.floor((Math.random() * 7)) + 1;
+    setRandInt(randInt);
+    console.log(randInt);
+  }
+
+
+
+  const determineColor = () => {
+    generateRandInt();
+    let colors = ["magenta", "orange", "green", "blue", "yellow", "black", "pink"];
+    colors.forEach((color) => {
+      if(randInt === colors.indexOf(color)){
+        setRandColor(color);
+      }
+    })
+
+    console.log(randColor)
+  }
 
   const addOne = () => {
-    setState({count: state.count += 1})
+    setCount(count + 1)
   }
 
   const subtractOne = () => {
-    setState({ count: state.count -= 1 })
+    setCount(count - 1)
   }
 
   let content = (
-    <React.Fragment> 
+    <div> 
       <h1 className="text-center pt-5">Counter</h1>
       <div className="row">
         <button onClick={subtractOne} style={{position: "absolute", top: "20%", left: "50%"}}>-</button>
         <button onClick={addOne} style={{position: "absolute", top: "20%", right: "50%"}}>+</button>
       </div>
+      
 
-      <h1 className="text-center">{state.count}</h1>
+      <h1 className="text-center" style={{color: `${randColor}`}}>{count}</h1>
+      <button onClick={determineColor} style={{position: "absolute", top: "50%", left: "45%", right: "45%"}}>Change color</button>
 
-    </React.Fragment>
+      
+
+    </div>
   )
 
   return content;
